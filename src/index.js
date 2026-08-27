d1b22b9fe3423a56fa124d5ede2e0169eb9b8647
seeun-jang <jangseeun108@gmail.com>
2026-08-27 11:47:18 +0900

    - local에서 파일로 직접실행(CDN 방식으로 HTML 문서에 라이브러리 import) - npm 서버를 사용해서 JSX 파일 작성해서 실행

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+//import TodoListApp from "./01/TodoListApp";
 import reportWebVitals from './reportWebVitals';
+import Library from "./03/Library";
+import "./03/Book.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
